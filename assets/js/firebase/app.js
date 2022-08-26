29569149a1030e6db90a9f450dac71878f38dd32
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-26 08:22:54 -0300

    firebase config

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6xKyJrBePvNua4lOnbC-XGgBjusKYDjI",
+  authDomain: "hellfire-club-subscription.firebaseapp.com",
+  projectId: "hellfire-club-subscription",
+  storageBucket: "hellfire-club-subscription.appspot.com",
+  messagingSenderId: "340054227188",
+  appId: "1:340054227188:web:d4f5b89e751698646306a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
