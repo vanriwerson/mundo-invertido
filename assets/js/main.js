b9e677f52d366bc352bdec3358469b90ca210707
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-26 08:01:22 -0300

    recuperando dados do form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 import app from './firebase/app';
 
 const txtName = document.getElementById('txtName');
+const txtEmail = document.getElementById('txtEmail');
+const txtLevel = document.getElementById('txtLevel');
+const txtCharacter = document.getElementById('txtCharacter');
+
+const btnSubscribe = document.getElementById('btnSubscribe');
+
+btnSubscribe.addEventListener('click', () => {
+  const subscription = {
+    name: txtName.value,
+    email: txtEmail.value,
+    level: txtLevel.value,
+    character: txtCharacter.value,
+  };
+
+});
